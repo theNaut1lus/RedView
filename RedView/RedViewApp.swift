0ac938c4468e1d4dc1a4627e3b67167dfb2fcf1d
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-03-26 18:28:06 +1100

    Initial Commit

---
+++ RedView/RedViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedViewApp.swift
+//  RedView
+//
+//  Created by Sidak Singh Aulakh on 26/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct RedViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
