d3ef75b936d862e257922fbc065a0c06263328fe
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-04-01 20:14:46 +1100

    Class 26/03

--- RedView/ContentView.swift
+++ RedView/ContentView.swift
@@ -8,14 +8,81 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var step = 22
+    @State var color: Color = .color
+    
+    var lyrics = [
+        "Missing him was",
+        "Loving him was"
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .fill(color)
+                .ignoresSafeArea()
+            VStack (alignment: .leading) {
+                Text("I DONT KNOW ABOUT YOU")
+                    .foregroundStyle(.regularMaterial)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.black)
+                HStack{
+                    Text("BUT I'M FEELING")
+                        .foregroundStyle(.regularMaterial)
+//                        .foregroundColor(.chartruse)
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .fontWeight(.black)
+                    Stepper(value: $step, in: 0...50) {
+                        Text(String(step) + "!")
+                    }
+                    .foregroundStyle(.regularMaterial)
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.black)
+                }
+                VStack {
+                    HStack{
+                        ColorPicker("LOVING HIM WAS", selection: $color)
+                            .foregroundStyle(.regularMaterial)
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .fontWeight(.black)
+                    }
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .fill(.red)
+                            .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                }
+                HStack {
+                    Button(action: {
+                        //nothing
+                    }, label: {
+                        RoundedRectangle(cornerRadius: 10.0)
+                            .fill(.blue)
+                            .frame(height: 50)
+                    })
+                    Button(action: {
+                        //nothing
+                    }, label: {
+                        RoundedRectangle(cornerRadius: 10.0)
+                            .fill(.red)
+                            .frame(height: 50)
+                    })
+                    Button(action: {
+                        //nothing
+                    }, label: {
+                        RoundedRectangle(cornerRadius: 10.0)
+                            .fill(.gray)
+                            .frame(height: 50)
+                    })
+                    Spacer()
+                }
+            }
+            .padding()
         }
-        .padding()
+    }
+}
+
+extension Color {
+    static var chartruse: Color {
+        return Color(red: 1.0, green: 0.5, blue: 0.5)
     }
 }
 
